ab48df1f8699f92f9e8f97db844b095a9c9dd4f4
Santiago Rendon <santirg2000@gmai.com>
2021-04-11 18:24:41 -0500

    Multichain CLI scripts

---
+++ cli-scripts/smart_filters.js
@@ -0,0 +1,25 @@
+// Smart filter para stream vendedores.
+function filterstreamitem() {
+  const item = getfilterstreamitem();
+  if (item.keys.length != 1) return "Debe tener una sola clave.";
+  if (item.format != "json") return "Debe ser de tipo JSON.";
+
+  const json = item.data.json;
+  if (!json.hasOwnProperty("nombre")) return "Debe tener el atributo nombre.";
+  if (!json.hasOwnProperty("telefono"))
+    return "Debe tener el atributo telefono.";
+  if (Object.keys(json).length > 2)
+    return "Solo puede incluir los atributos nombre y telefono";
+}
+
+// Smart filter para stream ganancias.
+function filterstreamitem() {
+  const item = getfilterstreamitem();
+  if (item.keys.length != 1) return "Debe tener una sola clave.";
+  if (item.format != "json") return "Debe ser de tipo JSON.";
+
+  const json = item.data.json;
+  if (!json.hasOwnProperty("valor")) return "Debe tener el atributo valor.";
+  if (Object.keys(json).length > 1)
+    return "Solo puede incluir el atributo valor";
+}
